1674df1e1a9207e3e665150f777ef9dca5c48c28
boddy <boddy@DESKTOP-DLSUEFA>
2021-08-06 19:21:32 +0800

    java

---
+++ spring20210802/src/main/java/com/study/spring/case01/MainTest.java
@@ -0,0 +1,14 @@
+package com.study.spring.case01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctxApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+
+	}
+
+}
